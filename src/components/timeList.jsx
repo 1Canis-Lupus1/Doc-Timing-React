44d31c7e30155a9d6c180e40971011e63eeb9652
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2020-10-09 02:45:01 +0530

    initial commit

---
+++ src/components/timeList.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const TimeList=(props)=>{
+    // {console.log(props)}
+    return(
+        // props.timing.map((val,index)=>{
+        //     let timingStart=`timingStart-${index}`;
+        //     let timingEnd=`timingEnd-${index}`;
+        //     console.log(timingStart)
+        // })
+    )
+        
+}
+
+export default TimeList;
